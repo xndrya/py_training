9620e24ff8c65e3a7fbeaebf3145f1b70c479db0
X <xndrya@gmail.com>
2019-06-02 20:58:38 +0300

    Выделение вспомогательных методов в тесте создания нового контакта

--- add_new_contact.py
+++ add_new_contact.py
@@ -14,24 +14,29 @@ class AddNewContact(unittest.TestCase):
         # open home page
         wd.get("http://localhost/addressbook/")
         # login
-        wd.find_element_by_xpath("//input[@name='user']").clear()
-        wd.find_element_by_xpath("//input[@name='user']").send_keys("admin")
-        wd.find_element_by_xpath("//input[@name='pass']").clear()
-        wd.find_element_by_xpath("//input[@name='pass']").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+        self.login(wd, "admin", "secret")
         # create contact
+        self.create_contact(wd, "First name", "Last name", "Address", "1234567890")
+
+    def create_contact(self, wd, firstname, lastname, address, home_phone):
         wd.find_element_by_xpath("//a[contains(text(),'add new')]").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("First name")
+        wd.find_element_by_name("firstname").send_keys(firstname)
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Last name")
+        wd.find_element_by_name("lastname").send_keys(lastname)
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("Address")
+        wd.find_element_by_name("address").send_keys(address)
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("1234567890")
-        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("home").send_keys(home_phone)
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-    
+
+    def login(self, wd, username, password):
+        wd.find_element_by_xpath("//input[@name='user']").clear()
+        wd.find_element_by_xpath("//input[@name='user']").send_keys(username)
+        wd.find_element_by_xpath("//input[@name='pass']").clear()
+        wd.find_element_by_xpath("//input[@name='pass']").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
--- group.py
+++ group.py
@@ -4,5 +4,3 @@ class Group:
         self.group_name=group_name
         self.group_header=group_header
         self.group_footer=group_footer
-
-
