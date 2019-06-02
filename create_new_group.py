fbb7c631e2d237108f0387dd28db4b95ad40926c
X <xndrya@gmail.com>
2019-06-02 19:42:21 +0300

    Выделение действий в методы

--- create_new_group.py
+++ create_new_group.py
@@ -11,22 +11,44 @@ class UntitledTestCase(unittest.TestCase):
 
     def test_untitled_test_case(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        wd.find_element_by_xpath("//input[@name='user']").clear()
-        wd.find_element_by_xpath("//input[@name='user']").send_keys("admin")
-        wd.find_element_by_xpath("//input[@name='pass']").clear()
-        wd.find_element_by_xpath("//input[@name='pass']").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        wd.find_element_by_xpath("//a[contains(text(),'groups')]").click()
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_xpath("//a[contains(@href, '#')]").click()
+
+    def return_to_groups_page(self, wd):
+        wd.find_element_by_xpath("//a[contains(text(),'group page')]").click()
+
+    def create_group(self, wd):
+        # new group creation
         wd.find_element_by_xpath("//input[@name='new']").click()
+        # fill group form
         wd.find_element_by_xpath("//input[@name='group_name']").clear()
         wd.find_element_by_xpath("//input[@name='group_name']").send_keys("New group")
         wd.find_element_by_xpath("//textarea[@name='group_header']").clear()
         wd.find_element_by_xpath("//textarea[@name='group_header']").send_keys("This is a new group in this Address Book")
         wd.find_element_by_xpath("//textarea[@name='group_footer']").clear()
         wd.find_element_by_xpath("//textarea[@name='group_footer']").send_keys("This is a comment")
+        # submit form
         wd.find_element_by_xpath("//input[@name='submit']").click()
-        wd.find_element_by_xpath("//a[contains(text(),'group page')]").click()
+
+    def open_groups_page(self, wd):
+        wd.find_element_by_xpath("//a[contains(text(),'groups')]").click()
+
+    def login(self, wd):
+        wd.find_element_by_xpath("//input[@name='user']").clear()
+        wd.find_element_by_xpath("//input[@name='user']").send_keys("admin")
+        wd.find_element_by_xpath("//input[@name='pass']").clear()
+        wd.find_element_by_xpath("//input[@name='pass']").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
 
     def is_element_present(self, how, what):
         try:
