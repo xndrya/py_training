7f52ad54fc6f170d2a369abf91272d0fd7230a13
X <xndrya@gmail.com>
2019-06-02 20:51:20 +0300

    Подготовка к выделению вспомогательных методов

--- add_new_contact.py
+++ add_new_contact.py
@@ -6,37 +6,44 @@ import unittest
 
 class AddNewContact(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
+        self.wd = webdriver.Firefox(executable_path ="C:\geckodriver\geckodriver.exe")
+        self.wd.implicitly_wait(30)
     
     def test_add_new_contact(self):
-        driver = self.driver
-        driver.get("http://localhost/addressbook/")
-        driver.find_element_by_xpath("//input[@value='Login']").click()
-        driver.find_element_by_xpath("//a[contains(text(),'add new')]").click()
-        driver.find_element_by_name("firstname").clear()
-        driver.find_element_by_name("firstname").send_keys("First name")
-        driver.find_element_by_name("lastname").clear()
-        driver.find_element_by_name("lastname").send_keys("Last name")
-        driver.find_element_by_name("address").clear()
-        driver.find_element_by_name("address").send_keys("Address")
-        driver.find_element_by_name("home").clear()
-        driver.find_element_by_name("home").send_keys("1234567890")
-        driver.find_element_by_name("address2").click()
-        driver.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        wd = self.wd
+        # open home page
+        wd.get("http://localhost/addressbook/")
+        # login
+        wd.find_element_by_xpath("//input[@name='user']").clear()
+        wd.find_element_by_xpath("//input[@name='user']").send_keys("admin")
+        wd.find_element_by_xpath("//input[@name='pass']").clear()
+        wd.find_element_by_xpath("//input[@name='pass']").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+        # create contact
+        wd.find_element_by_xpath("//a[contains(text(),'add new')]").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys("First name")
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys("Last name")
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys("Address")
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys("1234567890")
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
     
     def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
+        try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
     
     def is_alert_present(self):
-        try: self.driver.switch_to_alert()
+        try: self.wd.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
     
     def tearDown(self):
-        self.driver.quit()
+        self.wd.quit()
 
 if __name__ == "__main__":
     unittest.main()
