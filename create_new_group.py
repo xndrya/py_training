b2d988006f3543ee2465c807e333656d411af1f8
X <xndrya@gmail.com>
2019-06-02 20:21:28 +0300

    Параметризация вспомогательных методов

--- create_new_group.py
+++ create_new_group.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 import unittest
+from group import Group
 from selenium import webdriver
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import NoSuchElementException
 
-class UntitledTestCase(unittest.TestCase):
+class test_add_new_group(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox(executable_path ="C:\geckodriver\geckodriver.exe")
         self.wd.implicitly_wait(30)
@@ -12,9 +13,9 @@ class UntitledTestCase(unittest.TestCase):
     def test_untitled_test_case(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd)
+        self.create_group(wd, Group("New group", "This is a new group in this Address Book", "This is a comment"))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
@@ -24,27 +25,27 @@ class UntitledTestCase(unittest.TestCase):
     def return_to_groups_page(self, wd):
         wd.find_element_by_xpath("//a[contains(text(),'group page')]").click()
 
-    def create_group(self, wd):
+    def create_group(self, wd, group):
         # new group creation
         wd.find_element_by_xpath("//input[@name='new']").click()
         # fill group form
         wd.find_element_by_xpath("//input[@name='group_name']").clear()
-        wd.find_element_by_xpath("//input[@name='group_name']").send_keys("New group")
+        wd.find_element_by_xpath("//input[@name='group_name']").send_keys(group.group_name)
         wd.find_element_by_xpath("//textarea[@name='group_header']").clear()
-        wd.find_element_by_xpath("//textarea[@name='group_header']").send_keys("This is a new group in this Address Book")
+        wd.find_element_by_xpath("//textarea[@name='group_header']").send_keys(group.group_header)
         wd.find_element_by_xpath("//textarea[@name='group_footer']").clear()
-        wd.find_element_by_xpath("//textarea[@name='group_footer']").send_keys("This is a comment")
+        wd.find_element_by_xpath("//textarea[@name='group_footer']").send_keys(group.group_footer)
         # submit form
         wd.find_element_by_xpath("//input[@name='submit']").click()
 
     def open_groups_page(self, wd):
         wd.find_element_by_xpath("//a[contains(text(),'groups')]").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_xpath("//input[@name='user']").clear()
-        wd.find_element_by_xpath("//input[@name='user']").send_keys("admin")
+        wd.find_element_by_xpath("//input[@name='user']").send_keys(username)
         wd.find_element_by_xpath("//input[@name='pass']").clear()
-        wd.find_element_by_xpath("//input[@name='pass']").send_keys("secret")
+        wd.find_element_by_xpath("//input[@name='pass']").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self, wd):
---
+++ group.py
@@ -0,0 +1,8 @@
+class Group:
+
+    def __init__(self, group_name, group_header, group_footer):
+        self.group_name=group_name
+        self.group_header=group_header
+        self.group_footer=group_footer
+
+
