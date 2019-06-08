c79cc38b7a64635536905e7105b7afa6b460e667
X <xndrya@gmail.com>
2019-06-08 14:31:59 +0300

    Переход на pytest и выделение отдельного класса для методов в тесте для добавления нового контакта

--- add_new_contact.py
+++ add_new_contact.py
@@ -1,52 +1,15 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
+import pytest
+from application import Application
 from contact import Contact
 
-class AddNewContact(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox(executable_path ="C:\geckodriver\geckodriver.exe")
-        self.wd.implicitly_wait(30)
-    
-    def test_add_new_contact(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        self.login(wd, "admin", "secret")
-        self.create_contact(wd, Contact("First name", "Last name", "Address", "1234567890"))
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def create_contact(self, wd, contact):
-        wd.find_element_by_xpath("//a[contains(text(),'add new')]").click()
-        wd.find_element_by_xpath("//input[@name='firstname']").clear()
-        wd.find_element_by_xpath("//input[@name='firstname']").send_keys(contact.firstname)
-        wd.find_element_by_xpath("//input[@name='lastname']").clear()
-        wd.find_element_by_xpath("//input[@name='lastname']").send_keys(contact.lastname)
-        wd.find_element_by_xpath("//textarea[@name='address']").clear()
-        wd.find_element_by_xpath("//textarea[@name='address']").send_keys(contact.address)
-        wd.find_element_by_xpath("//input[@name='home']").clear()
-        wd.find_element_by_xpath("//input[@name='home']").send_keys(contact.home_phone)
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-
-    def login(self, wd, username, password):
-        wd.find_element_by_xpath("//input[@name='user']").clear()
-        wd.find_element_by_xpath("//input[@name='user']").send_keys(username)
-        wd.find_element_by_xpath("//input[@name='pass']").clear()
-        wd.find_element_by_xpath("//input[@name='pass']").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == "__main__":
-    unittest.main()
+def test_add_new_contact(app):
+    app.open_home_page()
+    app.login(username="admin", password="secret")
+    app.create_contact(Contact("First name", "Last name", "Address", "1234567890"))
--- application.py
+++ application.py
@@ -44,5 +44,18 @@ class Application:
         wd = self.wd
         wd.get("http://localhost/addressbook/")
 
+    def create_contact(self, contact):
+        wd = self.wd
+        wd.find_element_by_xpath("//a[contains(text(),'add new')]").click()
+        wd.find_element_by_xpath("//input[@name='firstname']").clear()
+        wd.find_element_by_xpath("//input[@name='firstname']").send_keys(contact.firstname)
+        wd.find_element_by_xpath("//input[@name='lastname']").clear()
+        wd.find_element_by_xpath("//input[@name='lastname']").send_keys(contact.lastname)
+        wd.find_element_by_xpath("//textarea[@name='address']").clear()
+        wd.find_element_by_xpath("//textarea[@name='address']").send_keys(contact.address)
+        wd.find_element_by_xpath("//input[@name='home']").clear()
+        wd.find_element_by_xpath("//input[@name='home']").send_keys(contact.home_phone)
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+
     def destroy(self):
         self.wd.quit()
