77cde4e8b7cbfe5962139a201770219977bdc673
X <xndrya@gmail.com>
2019-06-08 21:41:11 +0300

    реализация метода для редактирования групп

--- fixture/group.py
+++ fixture/group.py
@@ -26,6 +26,20 @@ class GroupHelper:
         wd.find_element_by_xpath("(//*[@name='selected[]'])[1]").click()
         wd.find_element_by_xpath("//*[@name='delete']").click()
 
+    def edit_first_group(self, group):
+        wd = self.app.wd
+        wd.find_element_by_xpath("(//*[@name='selected[]'])[1]").click()
+        wd.find_element_by_xpath("//*[@name='edit']").click()
+        wd.find_element_by_xpath("//input[@name='group_name']").clear()
+        wd.find_element_by_xpath("//input[@name='group_name']").send_keys(group.group_name)
+        wd.find_element_by_xpath("//textarea[@name='group_header']").clear()
+        wd.find_element_by_xpath("//textarea[@name='group_header']").send_keys(group.group_header)
+        wd.find_element_by_xpath("//textarea[@name='group_footer']").clear()
+        wd.find_element_by_xpath("//textarea[@name='group_footer']").send_keys(group.group_footer)
+        wd.find_element_by_xpath("//*[@name='update']").click()
+
+
+
     def open_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_xpath("//a[contains(text(),'groups')]").click()
--- tests/test_edit_first_group.py
+++ tests/test_edit_first_group.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
-def test_delete_first_group(app):
+from model.group import Group
+
+def test_edit_first_group(app):
     app.open_home_page()
     app.session.login(username="admin", password="secret")
     app.group.open_groups_page()
-    app.group.delete_first_group()
+    app.group.edit_first_group(Group("Edited group", "This is the edited group in this Address Book", "This is the edited comment"))
     app.group.return_to_groups_page()
     app.session.logout()
