dba7021870658756c25eb8ef49f924498a0b9f4e
X <xndrya@gmail.com>
2019-06-08 16:25:15 +0300

    поправил имена тестов

--- tests/test_create_new_group.py
+++ tests/test_create_new_group.py
@@ -2,7 +2,7 @@
 
 from model.group import Group
 
-def test_untitled_test_case(app):
+def test_create_new_group(app):
     app.open_home_page()
     app.session.login(username="admin", password="secret")
     app.group.open_groups_page()
