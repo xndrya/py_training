307709cd80d3655c09cdf05af686e8d53f6a4458
X <xndrya@gmail.com>
2019-06-08 14:59:36 +0300

    Реализация пакетов для фикстур, моделей, тестов

--- tests/add_new_contact.py
+++ tests/add_new_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from application import Application
-from contact import Contact
+from fixture.application import Application
+from model.contact import Contact
 
 @pytest.fixture
 def app(request):
