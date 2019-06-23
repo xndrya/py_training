453b24110664df6ce0a97391de805e110f349b2b
X <xndrya@gmail.com>
2019-06-23 20:50:59 +0300

    Сортировка списка групп для сравнения

--- model/group.py
+++ model/group.py
@@ -1,3 +1,5 @@
+from sys import maxsize
+
 class Group:
 
     def __init__(self, group_name=None, group_header=None, group_footer=None, id = None):
@@ -5,3 +7,15 @@ class Group:
         self.group_header=group_header
         self.group_footer=group_footer
         self.id = id
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.group_name)
+
+    def __eq__(self, other):
+        return (self.id is None or other.id is None or self.id == other.id) and self.group_name == other.group_name
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
