fe7c64227804c074e8014d534c754e9c780069cc
X <xndrya@gmail.com>
2019-06-23 18:25:29 +0300

    Избавился от избыточных логинов/логаутов

--- tests/test_add_new_contact.py
+++ tests/test_add_new_contact.py
@@ -4,6 +4,4 @@ from model.contact import Contact
 
 def test_add_new_contact(app):
     app.open_home_page()
-    # app.session.login(username="admin", password="secret")
     app.contact.create(Contact("First name", "Last name", "Address", "1234567890"))
-    # app.session.logout()
--- tests/test_delete_group.py
+++ tests/test_delete_group.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
+
 from model.group import Group
+
 def test_delete_first_group(app):
     app.open_home_page()
-    # app.session.login(username="admin", password="secret")
     app.group.open_groups_page()
     if app.group.count == 0:
         app.group.create(Group(group_name="test", group_header="test header", group_footer="test_footer"))
     app.group.delete_first_group()
     app.group.return_to_groups_page()
-    # app.session.logout()
