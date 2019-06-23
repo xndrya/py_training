85ebb54da350b86fdba8e14ecadd2aa13610367f
X <xndrya@gmail.com>
2019-06-23 22:08:50 +0300

    Сортировка списка контактов по ключу

--- model/contact.py
+++ model/contact.py
@@ -1,3 +1,5 @@
+from sys import maxsize
+
 class Contact:
 
     def __init__(self, firstname=None, lastname=None, address=None, home_phone=None, id=None):
@@ -8,7 +10,13 @@ class Contact:
         self.id = id
 
     def __repr__(self):
-        return "%s:%s" % (self.id, self.lastname)
+        return "%s:%s:%s" % (self.id, self.lastname, self.firstname)
 
     def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id) and self.lastname == other.lastname
+        return (self.id is None or other.id is None or self.id == other.id) and self.lastname == other.lastname and self.firstname == other.firstname
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
