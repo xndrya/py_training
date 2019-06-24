cc1395437c870d8051ed86cce46e6f3c44bde3b2
X <xndrya@gmail.com>
2019-06-24 23:53:18 +0300

    Исправления проверки равенства списков групп, контактов

--- tests/test_delete_contact.py
+++ tests/test_delete_contact.py
@@ -13,4 +13,4 @@ def test_delete_some_contact(app):
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) - 1 == len(new_contacts)
     old_contacts[index:index+1] = []
-    assert old_contacts == new_contacts
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
--- tests/test_delete_group.py
+++ tests/test_delete_group.py
@@ -14,4 +14,4 @@ def test_delete_some_group(app):
     new_groups = app.group.get_group_list()
     assert len(old_groups) - 1 == len(new_groups)
     old_groups[index:index+1] = []
-    assert old_groups == new_groups
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
