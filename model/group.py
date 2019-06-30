7264a6c7b68e2163e3cadbb272854d97a396d24c
X <xndrya@gmail.com>
2019-06-30 20:33:56 +0300

    Параметризация для групп

--- model/group.py
+++ model/group.py
@@ -9,7 +9,7 @@ class Group:
         self.id = id
 
     def __repr__(self):
-        return "%s:%s" % (self.id, self.group_name)
+        return "%s:%s:%s:%s" % (self.id, self.group_name, self.group_header, self.group_footer)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.group_name == other.group_name
--- tests/test_create_new_group.py
+++ tests/test_create_new_group.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
-
+import pytest
+import random
+import string
 from model.group import Group
 
-def test_create_new_group(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters+string.digits+" "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [Group("", "", "")]+[
+    Group(random_string("name",10), random_string("header",20), random_string("footer",20)) for i in range(5)
+]
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_create_new_group(app, group):
     app.open_home_page()
     app.group.open_groups_page()
+    # group = Group("New group", "This is a new group in this Address Book", "This is a comment")
     old_groups = app.group.get_group_list()
-    group = Group("New group", "This is a new group in this Address Book", "This is a comment")
     app.group.create(group)
     assert len(old_groups)+1 == app.group.count()
     new_groups = app.group.get_group_list()
