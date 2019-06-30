ead03c94cd86892a9f3575bb7a5b98b930991d06
X <xndrya@gmail.com>
2019-06-30 17:53:56 +0300

    Исправления для метода получения списка контактов

--- fixture/application.py
+++ fixture/application.py
@@ -2,20 +2,29 @@ from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
+
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox(executable_path ="C:\geckodriver\geckodriver.exe")
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox(executable_path ="C:\geckodriver\geckodriver.exe")
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie
+        else:
+            raise ValueError("Unrecognized type of browser - %s" % browser)
         self.wd.implicitly_wait(1)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def open_home_page(self):
         wd = self.wd
         if wd.current_url.endswith("/addressbook") and wd.find_elements_by_xpath("//*[@id='maintable']/tbody/tr[1]/th[2]/a").text == "Last name":
             return
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
